46ea4ab75f947f1b91b1eab9535e47b3acda394d
Tomasz Gawlik <tomaszgawlik00@gmail.com>
2026-02-17 22:42:37 +0100

    expect IRQ to be reliable

--- src/input/UpDownInterruptBase.cpp
+++ src/input/UpDownInterruptBase.cpp
@@ -146,7 +146,7 @@ int32_t UpDownInterruptBase::runOnce()
         this->action = UPDOWN_ACTION_NONE;
     }
 
-    return 20; // This will control how the input frequency
+    return INT32_MAX; // This will control how the input frequency
 }
 
 void UpDownInterruptBase::intPressHandler()
